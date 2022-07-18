3266b994df17b10150fd5b6e161fc22ffeed996d
rodrigox98 <rodrigo.silva31@hotmail.com>
2022-07-18 09:51:41 -0300

    Exercício

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite o número para calcular a tabuada");
+            numero = int.Parse(Console.ReadLine());
+            for(int i = 0; i <=10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero*i}");
+            }
+        }
+    }
+}
